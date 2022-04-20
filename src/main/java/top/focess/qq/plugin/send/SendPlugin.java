64e7a60a7488e7f6b0c0dd69fbc377cc09fc38a4
MidCoard <2624646185@qq.com>
2022-04-20 09:41:37 +0800

    update to 4.1.2.0000

--- src/main/java/top/focess/qq/plugin/send/SendPlugin.java
+++ src/main/java/top/focess/qq/plugin/send/SendPlugin.java
@@ -3,6 +3,6 @@ package top.focess.qq.plugin.send;
 import top.focess.qq.api.plugin.LazyPlugin;
 import top.focess.qq.api.plugin.PluginType;
 
-@PluginType(name = "SendPlugin",version = "3.0.0")
+@PluginType
 public class SendPlugin extends LazyPlugin {
 }
